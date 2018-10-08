a2ac91518d48d2b010723b08b7c9412f46a9e520
JaredDobry <jareddobry10@gmail.com>
2018-10-08 10:41:13 -0400

    Update Main.cpp
    Now generates multiple files, one for each set of peak data.
    Peak data now adds a few extra (5) data points of the noise on both sides of the main curve.
    Peak data is sorted by largest intensity to smallest, so Peak0.txt will always be the most intense curve.
    Added option to output only a certain number of peaks, say 5 out of 10 total. (largest intensity comes first)

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,6 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
@@ -13,16 +16,16 @@ int main()
 	cin >> in;
 	minimumValueReq = stod(in);
 	vector<pair<double, double>> dataMap;
+	vector<pair<double, vector<pair<double, double>>>> peakMap;
 	int curPeakIndex = 1;
-	string totalOutString = "";
-	const char* fileWritePath = "C:/Data/DataOut.txt";
+	string fileWritePath = "C:/Data/";
 	const char* fileReadPath = "C:/Data/DataIn.txt";
 	ifstream inFile;
 	inFile.open(fileReadPath);
 	//Do read in here and data analysis
 	string s;
 	//Read all data into our vector
-	while (!inFile.eof())
+	while (!inFile.eof()) 
 	{
 		getline(inFile, s);
 		size_t spaceLocation = s.find_first_of("\t");
@@ -39,6 +42,7 @@ int main()
 
 	for (int it = 0; it < dataMap.size() - 2; it++) //-2 to deal with checking next 2 neighbors
 	{
+		//Check if we are on positive slope or not 
 		//Search for things above the cutoff
 		if (dataMap.at(it).first > minimumValueReq)
 		{
@@ -76,20 +80,82 @@ int main()
 						it3++;
 					}
 				}
+				//Add some of the outside low values for better fitting
+				firstInSeries -= 5;
+				if (firstInSeries < 0)
+					firstInSeries = 0;
+
+				secondInSeries += 5;
+				if (secondInSeries > dataMap.size() - 1)
+					secondInSeries = dataMap.size() - 1;
+
 				//Place into output string
 				//totalOutString += "Peak " + to_string(curPeakIndex) + "\n";
 				curPeakIndex++;
+				double maxVal = 0;
+				vector<pair<double, double>> peakData;
 				for (int j = firstInSeries; j <= secondInSeries; j++)
 				{
-					totalOutString += to_string(dataMap.at(j).first) + " " + to_string(dataMap.at(j).second) + "\n";
+					if (dataMap.at(j).first > maxVal)
+						maxVal = dataMap.at(j).first;
+					peakData.push_back(pair<double, double>(dataMap.at(j).first, dataMap.at(j).second));
 				}
+				peakMap.push_back(pair<double, vector<pair<double, double>>>(maxVal, peakData));
 				it = secondInSeries;
 			}
 		}
 	}
-	ofstream outFile;
-	outFile.open(fileWritePath);
-	outFile << totalOutString;
-	outFile.close();
+	//Sort peaks by largest size
+	vector<pair<double, vector<pair<double, double>>>> sortedMap;
+	vector<int> usedIndex;
+	while (sortedMap.size() < peakMap.size())
+	{
+		int index;
+		double largest = 0;
+		for (int i = 0; i < peakMap.size(); i++)
+		{
+			if (peakMap.at(i).first > largest)
+			{
+				bool used = false;
+				//Check hasnt been used
+				for (int v : usedIndex)
+				{
+					if (v == i)
+						used = true;
+				}
+				if (used == false)
+				{
+					largest = peakMap.at(i).first;
+					index = i;
+				}
+			}
+		}
+		sortedMap.push_back(peakMap.at(index));
+		usedIndex.push_back(index);
+	}
+
+	cout << "Found " + to_string(peakMap.size()) + " peaks, how many peak files to output? (Sorted largest to smallest intensity) ";
+	int choice;
+	cin >> choice;
+	if (choice > peakMap.size())
+	{
+		cout << "Larger choice than number of peaks, defaulting to all peaks" << endl;
+		choice = peakMap.size();
+	}
+	else if (choice <= 0)
+		return 0;
+	//Output peaks to files here
+	for (int i = 0; i < choice; i++)
+	{
+		string out = "";
+		ofstream outFile;
+		outFile.open(fileWritePath + "Peak" + to_string(i) + ".txt");
+		for (pair<double, double> d : peakMap.at(i).second) 
+		{
+			out += to_string(d.first) + " " + to_string(d.second) + "\n";
+		}
+		outFile << out;
+		outFile.close();
+	}
 }
 
